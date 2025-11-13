f5877a4c2d1d0f62635bf132a927174e800786d2
Siha06 <65899583+Siha06@users.noreply.github.com>
2025-11-14 05:30:58 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mv $GITHUB_WORKSPACE/patch/footer.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer.ut
-mv $GITHUB_WORKSPACE/patch/footer_login.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
-mv $GITHUB_WORKSPACE/patch/997-mac2.sh package/base-files/files/etc/uci-defaults/997-mac2.sh
-#mv $GITHUB_WORKSPACE/patch/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+mv $GITHUB_WORKSPACE/patch/xy/footer.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer.ut
+mv $GITHUB_WORKSPACE/patch/xy/footer_login.ut feeds/luci/themes/luci-theme-argon/ucode/template/themes/argon/footer_login.ut
+mv $GITHUB_WORKSPACE/patch/xy/997-mac2.sh package/base-files/files/etc/uci-defaults/997-mac2.sh
+mv $GITHUB_WORKSPACE/patch/xy/10_system.js feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 IPQ_TARGET=$(grep -o 'CONFIG_TARGET_qualcommax_[^=]*' .config | sed -n 's/CONFIG_TARGET_qualcommax_//p' | head -n1)
 
 rm -rf .vermagic
